25861c2f7adefe266328929c98b45a3234765900
Mason Tsai <mason.c.tsai@gmail.com>
2026-02-17 15:13:19 -0800

    doglog is GOATED

--- src/main/java/frc/robot/subsystems/SuperSystem.java
+++ src/main/java/frc/robot/subsystems/SuperSystem.java
@@ -8,8 +8,6 @@ import java.util.function.Consumer;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants.SwerveDriveConstants.FieldPositions;
