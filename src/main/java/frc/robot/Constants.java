609a9e261f57eaa9ac82aeaa24e70c1d77c1a292
nerdherd <thenerdherd687@gmail.com>
2026-09-04 18:36:27 -0700

    changes to hood contant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -400,7 +400,7 @@ public final class Constants {
     public static final double kShootWithDistanceB = 31.60409; // b
 
     public static final double kShootWithDistanceHoodA = 0.593151;//0.87; // a
-    public static final double kShootWithDistanceHoodB = 30.64891; // b
+    public static final double kShootWithDistanceHoodB = 31.64891; // b
 
     public static final double kLookAheadRingDriveFactor = 0.3; // use to tune the ring drive
     public static final double kLookAheadFactor = 1.35; // use to tune shoot on the move left and right
